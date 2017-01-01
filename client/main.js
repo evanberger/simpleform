8e5d57403e63bf500eb03e89dfcc3fe8dd565110
evanberger <evan.berger@gmail.com>
2017-01-01 13:53:58 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -4,103 +4,30 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import Collapsible from 'react-collapsible';
+var {Route, Router, IndexRoute,
+  hashHistory, browserHistory} = require('react-router');
+var App = require('./components/App');
+var Header = require('./components/header');
 var Form = require('./components/form');
 var Table = require('./components/table');
-var BuildingForm = require('./components/Bldg_Form');
+var BuildingForm = require('./components/bldg_form');
+var LoadGraph = require('./components/load_graph');
+var Homepage = require('./components/homepage');
+var ProjectDetails = require('./components/details');
+var Versus = require('./components/versus');
 
 // Create a component
-var App = React.createClass({
-  getInitialState: function() {
-    return {
-      load: 0,
-      profile: '',
-      name: '',
-      existingRate: '',
-      iceStorageRate: '',
-      chillerType: '',
-      coolingMonths: 8,
-      chillerTonnage: 0,
-      standardChillerEfficiency: 0,
-      ddChillerEfficiency: 0,
-      iceMakingEfficiency: 0,
-      standardChillerCost: 0,
-      ddChillerCost: 0,
-      icebankCost: 0,
-      hxCost: 0,
-      rebate: 0,
-      addlIceCost: 0,
-      taxes: 0,
-      downsizePipeSavings: 0,
-      downsizeDuctSavings: 0,
-      roundDuctSavings: 0
-    };
-  },
-  handleFormSubmit: function(submittedData) {
-    var newData = {
-      load: submittedData.load,
-      profile: submittedData.profile
-    }
-    this.setState({load: newData.load * 100, profile: newData.profile});
-  },
-  handleBuildingFormSubmit: function(submittedData) {
-    var newData = {
-      name: submittedData.name,
-      existingRate: submittedData.existingRate,
-      iceStorageRate:submittedData.iceStorageRate,
-      chillerType:submittedData.chillerType,
-      coolingMonths: submittedData.coolingMonths,
-      chillerTonnage: submittedData.chillerTonnage,
-      standardChillerEfficiency: submittedData.standardChillerEfficiency,
-      ddChillerEfficiency: submittedData.ddChillerEfficiency,
-      iceMakingEfficiency:submittedData.iceMakingEfficiency,
-      standardChillerCost: submittedData.standardChillerCost,
-      ddChillerCost:submittedData.icemakingChillerCost,
-      icebankCost: submittedData.icebankCost,
-      hxCost:submittedData.hxCost,
-      rebate: submittedData.rebate,
-      addlIceCost: submittedData.addlIceCost,
-      downsizePipeSavings: submittedData.downsizePipeSavings,
-      downsizeDuctSavings: submittedData.downsizeDuctSavings,
-      roundDuctSavings: submittedData.roundDuctSavings
-    }
-    this.setState({
-      name: newData.name,
-      existingRate: newData.existingRate,
-      iceStorageRate:newData.iceStorageRate,
-      chillerType:newData.chillerType,
-      coolingMonths: newData.coolingMonths,
-      chillerTonnage: newData.chillerTonnage,
-      standardChillerEfficiency: newData.standardChillerEfficiency,
-      ddChillerEfficiency: newData.ddChillerEfficiency,
-      iceMakingEfficiency:newData.iceMakingEfficiency,
-      standardChillerCost: newData.standardChillerCost,
-      ddChillerCost:newData.icemakingChillerCost,
-      icebankCost: newData.icebankCost,
-      hxCost:newData.hxCost,
-      rebate: newData.rebate,
-      addlIceCost: newData.addlIceCost,
-      downsizePipeSavings: newData.downsizePipeSavings,
-      downsizeDuctSavings: newData.downsizeDuctSavings,
-      roundDuctSavings: newData.roundDuctSavings
-    });
-  },
-  render: function () {
-      return (
-      <div>
-        <div className="col-xs-6">
-          <Form onFormSubmit={this.handleFormSubmit} />
-           <div>-------------------</div>
-           <Table load={this.state.load} profile={this.state.profile} />
-        </div>
-        <div className="col-xs-6">
-          <BuildingForm onFormSubmit={this.handleBuildingFormSubmit} />
-        </div>
-      </div>
-    );
-    }
-  });
 
 // Render this component to the screen
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.container'));
+  ReactDOM.render(
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Homepage} />
+        <Route path="loadgraph" component={LoadGraph} />
+        <Route path="details" component={ProjectDetails} />
+        <Route path="versus" component={Versus} />
+      </Route>
+    </Router>
+, document.querySelector('.container'));
 });
